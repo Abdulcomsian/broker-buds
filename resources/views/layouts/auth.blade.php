a210df4de620423cdd040990eca29d82ae370b3b
Abdul Basit <a.basit@accrualhub.com>
2023-02-08 16:38:11 +0500

    Intitial Commit

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Register</title>
+  <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    />
+  @yield("css")
+</head>
+<body class="hold-transition register-page">
+    @yield('content')
+<!-- /.register-box -->
+
+<!-- jQuery -->
+<script src="{{asset('backend/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('backend/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('backend/dist/js/adminlte.min.js')}}"></script>
+@yield("script")
+</body>
+</html>
